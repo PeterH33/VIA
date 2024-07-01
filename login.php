9bd52ab8b6b55a16b46d14bd3e5d4646b6532fa3
Peter Hartnett <peter.hartnett@gmail.com>
2024-07-01 15:51:46 -0400

    adding login stubs

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     <title>VIA - login</title>
     <link rel="stylesheet" href="CSS/mainStyle.css">
 </head>
-<body>
+<body class="login-body">
     <header>
         <!-- site logo on the left -->
         <div class="site-logo">logo</div>
@@ -14,25 +14,25 @@
 
     <main>
         <div class="login-container">
+            <!-- Vertical stack of Login and sub heading -->
             <h2>Login</h2>
             <h3>Enter your login credentials</h3>
+            <!-- Form with two fields -->
             <form action="authLogin.php" method="post">
                 <input type="text" name="userName" id="userName" required>
                 <input type="text" name="password" id="password" required>
+                <!-- button login -->
                 <input class="small-link wide-link" type="submit" value="Submit">
             </form>
+            <!-- div for -------or---------- -->
             <h3>--or--</h3>
+            <!-- button to create account -->
             <a class="small-link wide-link" href="createAccount.php">Create Account</a>
         </div>
-        <!-- Vertical stack of Login and sub heading -->
 
-        <!-- Form with two fields -->
 
-        <!-- button login -->
 
-        <!-- div for -------or---------- -->
 
-        <!-- button to create account -->
     </main>
 
 </body>
