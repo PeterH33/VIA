626b16d02a05b43699254c54d9d245a6db35854f
Peter Hartnett <peter.hartnett@gmail.com>
2024-07-01 13:09:18 -0400

    Login framing

---
+++ createAccount.php
@@ -0,0 +1,24 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+    <header>
+        <!-- site logo on the left -->
+    </header>
+
+    <main>
+        <!-- Vertical stack of Login and sub heading -->
+
+        <!-- 4 Form fields -->
+
+        <!-- button to create account -->
+    </main>
+
+</body>
+
+</html>
---
+++ loginauth.php
@@ -0,0 +1,27 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+    <header>
+        <!-- site logo on the left -->
+    </header>
+
+    <main>
+        <!-- Vertical stack of Login and sub heading -->
+
+        <!-- Form with two fields -->
+
+        <!-- button login -->
+
+        <!-- div for -------or---------- -->
+
+        <!-- button to create account -->
+    </main>
+
+</body>
+
+</html>
