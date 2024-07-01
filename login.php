6b025f1857759147aa9df2b5fffd6b57da670190
Peter Hartnett <peter.hartnett@gmail.com>
2024-07-01 15:24:11 -0400

    adding login stubs

---
+++ login.php
@@ -0,0 +1,37 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+    <header>
+        <!-- site logo on the left -->
+        <div class="site-logo">logo</div>
+    </header>
+
+    <main>
+        <div class="login-container">
+            <h2>Login</h2>
+            <h3>Enter your login credentials</h3>
+            <form action="authLogin.php" method="post">
+                <input type="text" name="userName" id="userName" required>
+                <input type="text" name="password" id="password" required>
+                <input class="small-link wide-link" type="submit" value="Submit">
+            </form>
+        </div>
+        <!-- Vertical stack of Login and sub heading -->
+
+        <!-- Form with two fields -->
+
+        <!-- button login -->
+
+        <!-- div for -------or---------- -->
+
+        <!-- button to create account -->
+    </main>
+
+</body>
+
+</html>
