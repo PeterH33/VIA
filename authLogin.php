6ba3886235313143a69324e22955ac1b01365aaf
Peter Hartnett <peter.hartnett@gmail.com>
2024-07-02 11:36:10 -0400

    Moving sanitization to dbConn.php require

--- authLogin.php
+++ authLogin.php
@@ -1,12 +1,7 @@
 <!-- We have 3 states here, fail, approval needed, and success -->
 <?php
-    function sanitizeString($var)
-    {
-        $var = stripslashes($var);
-        $var = strip_tags($var);
-        $var = htmlentities($var);
-        return $var;
-    }
-
     require 'dbConn.php';
+
     $mysqli->close();
+
+?>
--- dbConn.php
+++ dbConn.php
@@ -1,4 +1,12 @@
 <?php
+function sanitizeString($var)
+{
+    $var = stripslashes($var);
+    $var = strip_tags($var);
+    $var = htmlentities($var);
+    return $var;
+}
+
 $mysqli = new mysqli("127.0.0.1", "viaDemon", "pword", "viaDB");
 
 if ($mysqli->connect_error){
