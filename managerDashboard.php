7b1f39c492318db0a02aacfb1ef19839b32f8028
Peter Hartnett <peter.hartnett@gmail.com>
2024-07-03 16:46:32 -0400

    Adding dashboard menu, and icons

--- managerDashboard.php
+++ managerDashboard.php
@@ -1,10 +1,49 @@
+<?php
+//Secure page check for login and permission
+session_start();
 
+if (!isset($_SESSION['userName'])){
+    header('Location: login.php');
+    exit;
+}
+
+if (!$_SESSION['isManager']){
+    header('Location: workerDashboard.php');
+    exit;
+}
+
+?>
 
 <!DOCTYPE html>
 <html>
-
+<head>
+    <link rel="stylesheet" href="CSS/mainStyle.css">
+</head>
 <body>
-    Manager Dashboard
-    <a class="small-link" href="logout.php">Logout</a>
+    <header>
+        
+        <!-- logo on the left -->
+        <div class="site-logo">logo</div>
+        
+        <!-- Manager icon portrait on right next to logout button-->
+
+        <!-- Logout button on the right -->
+        <a class="small-link" href="logout.php">Log Out</a>
+
+    </header>    
+
+    <main>
+        <!-- Menu -->
+        <div class="dash-menuContainer">
+            <nav class="dash-sidebar">
+                <ul>
+                    <li><a href="managerDashboard.php"><img src="ICONS/home.svg" alt="Home Icon" class="dash-icon">VIA Round</a></li>
+                    <li><a href="managerTaskList.php"><img src="ICONS/plus.svg" alt="Task Icon" class="dash-icon"> Task List</a></li>
+                    <li><a href="managerWorkers.php"><img src="ICONS/person.svg" alt="Worker Icon" class="dash-icon">Workers</a></li>
+                    <li><a href="managerSettings.php"><img src="ICONS/gear.svg" alt="Settings Icon" class="dash-icon">Settings</a></li>
+                </ul>
+            </nav>
+        </div>
+    </main>
 </body>
 </html>
