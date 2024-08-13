a743dc2ba59c059a6d8352c70bb566ed853d1137
Peter Hartnett <peter.hartnett@gmail.com>
2024-08-13 18:12:47 -0400

    WorkerDash work

--- workerDashboard.php
+++ workerDashboard.php
@@ -48,7 +48,7 @@ require_once 'sani.php';
         <!-- Carousel goes here -->
         <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
-                <div class="carousel-item active">
+                <div class="carousel-item">
                     <img src="IMAGES/q1.png" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
