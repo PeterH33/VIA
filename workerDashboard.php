8be807f16bf053f1dcb6e2e8ab13b451ea62f1f6
Peter Hartnett <peter.hartnett@gmail.com>
2024-08-13 17:40:02 -0400

    WorkerDash work

--- workerDashboard.php
+++ workerDashboard.php
@@ -42,7 +42,33 @@ require_once 'sani.php';
                 </div>
             </div>
         </div>
-        
+
+        <h3>Available Tasks</h3>
+
+        <!-- Carousel goes here -->
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="..." class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="..." class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="..." class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+
+        <!-- I need to fetch all of the current tasks and put them up as cards -->
 
 
         <?php include 'footer.php';?>
