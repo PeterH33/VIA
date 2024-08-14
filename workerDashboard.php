9d3ceb60370a9541f3593e4dd3de714e3549fd49
Peter Hartnett <peter.hartnett@gmail.com>
2024-08-14 13:00:09 -0400

    WorkerDash work

--- workerDashboard.php
+++ workerDashboard.php
@@ -80,36 +80,19 @@ require_once 'sani.php';
                 $mysqli->close();
             ?>
 
-            <div class="mySlides fade">
-                <div class="numbertext">1 / 3</div>
-                <img src="IMAGES/q1.png" style="width:100%">
-                <div class="text">Caption Text</div>
-            </div>
-
-            <div class="mySlides fade">
-                <div class="numbertext">2 / 3</div>
-                <img src="IMAGES/q2.png" style="width:100%">
-                <div class="text">Caption Two</div>
-            </div>
-
-            <div class="mySlides fade">
-                <div class="numbertext">3 / 3</div>
-                <img src="IMAGES/q3.png" style="width:100%">
-                <div class="text">Caption Three</div>
-            </div>
-
         <!-- Next and previous buttons -->
-            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+            
+            
         </div>
         <br>
 
         <!-- The dots/circles -->
         <div style="text-align:center">
-
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
             <span class="dot" onclick="currentSlide(1)"></span>
             <span class="dot" onclick="currentSlide(2)"></span>
             <span class="dot" onclick="currentSlide(3)"></span>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
         </div> 
         
 
