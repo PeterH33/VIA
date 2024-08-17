840f248cce7f9872c016e4728e0e51eebb35c00c
Peter Hartnett <peter.hartnett@gmail.com>
2024-08-17 11:08:25 -0400

    adding delete task sql call

--- deleteTask.php
+++ deleteTask.php
@@ -3,6 +3,19 @@ if (isset($_POST['taskId'])) {
     require 'dbDash.php';
     $taskId = $_POST['taskId'];
     
+    //Having this call second caused issues, lets see if it works if it goes first?
+    $SQLString = "DELETE FROM assignments WHERE taskId = ?";
+    $stmt = $mysqli->prepare($SQLString);
+    $stmt->bind_param('i', $taskId);
+    
+    if ($stmt->execute()) {
+        echo "Task deleted successfully";
+    } else {
+        echo "Error deleting task";
+    }
+    
+    $stmt->close();
+
     // Prepare and execute the SQL statement to delete the task
     $SQLString = "DELETE FROM tasks WHERE taskId = ?";
     $stmt = $mysqli->prepare($SQLString);
@@ -16,17 +29,6 @@ if (isset($_POST['taskId'])) {
     
     $stmt->close();
 
-    $SQLString = "DELETE FROM assignments WHERE taskId = ?";
-    $stmt = $mysqli->prepare($SQLString);
-    $stmt->bind_param('i', $taskId);
-    
-    if ($stmt->execute()) {
-        echo "Task deleted successfully";
-    } else {
-        echo "Error deleting task";
-    }
-    
-    $stmt->close();
 
     $mysqli->close();
 } else {
